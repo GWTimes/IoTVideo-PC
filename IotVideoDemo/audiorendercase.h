32e81824671780188fd103982a6d4ad108ff7310
技威时代科技有限公司 <ios@gwell.cc>
2020-03-05 17:20:53 +0800

    Create audiorendercase.h
    更换文件夹名

---
+++ IotVideoDemo/audiorendercase.h
@@ -0,0 +1,35 @@
+﻿#ifndef AUDIORENDERCASE_H
+#define AUDIORENDERCASE_H
+#include "IoTVideoPlayerCommon.h"
+
+class QAudioFormat;
+class QAudioOutput;
+class QIODevice;
+class AudioRenderCase:public AudioRender
+{
+public:
+    AudioRenderCase();
+    ~AudioRenderCase();
+    /**
+     * 初始化
+     */
+    void init(AVHeader *header);
+
+    /**
+     * 渲染一帧音频帧
+     * @param data
+     * @return
+     */
+    void frameUpdate(AVData *data);
+
+    /**
+     * 释放
+     */
+    void release();
+private:
+    QAudioFormat *_fmt;
+    QAudioOutput *_out;
+    QIODevice *_io;
+};
+
+#endif // AUDIORENDERCASE_H
